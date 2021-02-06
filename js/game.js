bfa03bc33e85cd4a14a49aba3df5125dd223a364
loona2jin <36562112+loona2jin@users.noreply.github.com>
2021-02-06 23:07:49 +0900

    Update game.js

--- js/game.js
+++ js/game.js
@@ -145,12 +145,13 @@ function handleComplete() {
 
 function soundComplete(event) {
     if(event.id == "bgm") {
-        var props = new createjs.PlayPropsConfig().set({interrupt: createjs.Sound.INTERRUPT_ANY, loop: -1, volume: 0.5})
-        bgm = createjs.Sound.play("bgm", props);
-        
-        addClickToPlay();
+        setTimeout(function() {
+            var props = new createjs.PlayPropsConfig().set({interrupt: createjs.Sound.INTERRUPT_ANY, loop: -1, volume: 0.5})
+            bgm = createjs.Sound.play("bgm", props);
+
+            addClickToPlay(); 
+        },1000);
     }
-    
 }
 
 function addClickToPause(e) {
