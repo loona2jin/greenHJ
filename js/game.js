f3d38021a15d7a1b7c871411e0047cbe83f1c764
loona2jin <36562112+loona2jin@users.noreply.github.com>
2021-02-07 17:26:20 +0900

    Update game.js

--- js/game.js
+++ js/game.js
@@ -403,10 +403,10 @@ function toast(string) {
 	toast.classList.contains("reveal") ?
         (clearTimeout(removeToast), removeToast = setTimeout(function () {
             document.getElementById("toast").classList.remove("reveal")
-        }, 4000)) :
+        }, 3000)) :
         removeToast = setTimeout(function () {
             document.getElementById("toast").classList.remove("reveal")
-        }, 4000)
+        }, 3000)
     toast.classList.add("reveal"),
         toast.innerText = string
 	}
